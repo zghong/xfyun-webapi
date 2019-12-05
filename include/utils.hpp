342fbc762ddbedd324a6fb00f649507f64a8af19
hzg0226 <1491537453@qq.com>
2019-12-05 21:06:10 +0800

    完成生成接口授权部分，以及与之相关的工具类函数

---
+++ include/utils.hpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <openssl/hmac.h>
+#include <boost/archive/iterators/base64_from_binary.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+
+using namespace std;
+
+/*
+* 获取当前时间戳，并格式化成rfc1123格式
+* 输入：
+* 输出：当前时间戳的rfc1123格式字符串。"Thu, 05 Dec 2019 09:54:17 CST"
+*/
+string get_time_rfc1123()
+{
+	time_t rawtime;
+	time(&rawtime);
+	char date[64];
+	strftime(date, sizeof(date), "%a, %d %b %Y %X %Z\n", localtime(&rawtime));
+
+	return string(date);
+}
+
+/*
+* hmac_sha256算法，利用key对data进行加密认证处理
+* 输入：data，待sha256的数据；key，mac的密钥
+* 输出：256位加密字符，为方便处理，输出时转换为了string。"e�FX��}U0���^V!D@�����`|v"
+*/
+string get_hmac_sha256(const string &data, const string &key)
+{
+	unsigned char *result = new unsigned char[40];
+	unsigned int result_len = 0;
+
+	HMAC_CTX *ctx = HMAC_CTX_new();
+	HMAC_Init_ex(ctx, key.c_str(), key.size(), EVP_sha256(), NULL);
+	HMAC_Update(ctx, (unsigned char *)data.c_str(), data.size());
+	HMAC_Final(ctx, result, &result_len);
+	HMAC_CTX_free(ctx);
+
+	// ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+	// 不可以return string(result)，string的构造函数不接受unsigned char *
+	// 不可以直接 return (char *)result
+	// 因为result后面还有空余字符，加密的值要严格控制在result_len的长度范围内
+	// ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+	return string((char *)result, result_len);
+}
+
+/*
+* base64编码算法
+* 输入：data，待编码数据
+* 输出：data被base64编码后的字符串。"KuSCRlicg30QVTChBdToXlYhREDPxdkDiKNgfHYiDWU="
+*/
+string get_base64_encode(const string &data)
+{
+	typedef boost::archive::iterators::base64_from_binary<boost::archive::iterators::transform_width<string::const_iterator, 6, 8>> Base64EncodeIterator;
+
+	stringstream result;
+
+	copy(Base64EncodeIterator(data.begin()), Base64EncodeIterator(data.end()), ostream_iterator<char>(result));
+	size_t equal_count = (3 - data.length() % 3) % 3;
+	for (size_t i = 0; i < equal_count; i++)
+	{
+		result.put('=');
+	}
+
+	return result.str();
+}
+
+/*
+* url编码算法，转义url中的特殊字符和中文字符等
+* 输入：url，待编码的url
+* 输出：被编码转义后的url
+*/
+string get_url_encode(const string &url)
+{
+	typedef unsigned char BYTE;
+	string result;
+	size_t len = url.length();
+	unsigned char *pBytes = (unsigned char *)url.c_str();
+	char szAlnum[2];
+	char szOther[4];
+	for (size_t i = 0; i < len; i++)
+	{
+		if (isalnum((BYTE)url[i]))
+		{
+			sprintf(szAlnum, "%c", url[i]);
+			result.append(szAlnum);
+		}
+		else if (isspace((BYTE)url[i]))
+		{
+			result.append("+");
+		}
+		else
+		{
+			sprintf(szOther, "%%%X%X", pBytes[i] >> 4, pBytes[i] % 16);
+			result.append(szOther);
+		}
+	}
+	return result;
+}
--- source/speech_recognition/speech_dictation/iat_ws_cpp_demo.cpp
+++ source/speech_recognition/speech_dictation/iat_ws_cpp_demo.cpp
@@ -1,10 +1,72 @@
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
 
+#include "../../../include/utils.hpp"
+
 using namespace std;
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 
+/*
+* 相关参数类
+*/
+class WSParam
+{
+public:
+    WSParam(string APPID, string APISecret, string APIKey, string AudioFile)
+        : APPID(APPID), APISecret(APISecret), APIKey(APIKey), AudioFile(AudioFile)
+    {
+        // # 公共参数(common)
+        // self.CommonArgs = {"app_id": self.APPID}
+        // # 业务参数(business)，更多个性化参数可在官网查看
+        // self.BusinessArgs = {"domain": "iat", "language": "zh_cn", "accent": "mandarin", "vinfo":1,"vad_eos":10000}
+    }
+
+    // 生成接口鉴权url
+    string create_url()
+    {
+        // 生成RFC1123格式的时间戳，"Thu, 05 Dec 2019 09:54:17 CST"
+        string date = get_time_rfc1123();
+        // string date = "Thu, 05 Dec 2019 09:54:17 CST";
+
+        // 拼接signature的原始字符串
+        string signature_origin = "host: iat-api.xfyun.cn\n";
+        signature_origin += "date: " + date + "\n";
+        signature_origin += "GET /v2/iat HTTP/1.1";
+
+        // 使用hmac-sha256算法结合apiSecret对signature_origin签名，获得签名后的摘要signature_sha
+        string signature_sha = get_hmac_sha256(signature_origin, APISecret);
+
+        // 使用base64编码对signature_sha进行编码获得最终的signature
+        string signature = get_base64_encode(signature_sha);
+
+        // 拼接authorization的原始字符串
+        char buf[1024];
+        sprintf(buf, "api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"",
+                this->APIKey.c_str(), "hmac-sha256", "host date request-line", signature.c_str());
+        string authorization_origin = string(buf);
+
+        // 再对authorization_origin进行base64编码获得最终的authorization参数
+        string authorization = get_base64_encode(authorization_origin);
+
+        // 对相关参数构成url，并进行url编码，生成最终鉴权url
+        sprintf(buf, "authorization=%s&date=%s&host=%s",
+                authorization.c_str(), date.c_str(), "iat-api.xfyun.cn");
+        string url = "ws://iat-api.xfyun.cn/v2/iat?" + get_url_encode(string(buf));
+
+        return url;
+    }
+
+private:
+    string APPID;
+    string APISecret;
+    string APIKey;
+    string AudioFile;
+};
+
+/*
+* 与服务器进行websocket通信的客户端类
+*/
 class WSClient
 {
 public:
@@ -79,14 +141,30 @@ private:
 
 int main(int argc, char *argv[])
 {
-    WSClient c;
+    /*
+    * 测试websocket通信
+    */
+    // WSClient c;
 
-    string uri = "ws://localhost:9002";
-    if (argc == 2)
-    {
-        uri = argv[1];
-    }
+    // string uri = "ws://localhost:9002";
+    // if (argc == 2)
+    // {
+    //     uri = argv[1];
+    // }
 
-    c.run(uri);
+    // c.run(uri);
 
+    /*
+    * 测试获取鉴权url
+    */
+    string APPID = "5dde2318";
+    string APISecret = "4d3dcdf6184d4aa4d987cecca7de3896";
+    string APIKey = "5d5fe0d6177df2afc8a03909b71259eb";
+    string AudioFile = "../../../resource/iat_pcm_16k.pcm";
+    WSParam wsParam(APPID, APISecret, APIKey, AudioFile);
+    string wsUrl = wsParam.create_url();
+
+    cout << wsUrl << endl;
+
+    return 0;
 }
